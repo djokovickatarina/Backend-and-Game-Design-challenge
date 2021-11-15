099daf85439a015f381378ea94ed3f87bf9eb3ce
Katarina Djokovic <64806982+djokovickatarina@users.noreply.github.com>
2021-11-15 23:43:45 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -37,7 +37,7 @@ public class Main {
 			System.out.println(
 					"Enter the players rating(as number) or any other character for the end, for " + name + " club:");
 			int j = 0;
-			while (true && j < n) {
+			while (j < n) {
 				playerRating = sc.next();
 				try {
 					int playerRatingInt = Integer.parseInt(playerRating);
